75f569d0d58e231d8ecc1a8f31ceb664e0aefc9e
Jungeun17 <kkhj826@naver.com>
2023-11-22 13:22:09 +0900

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+USE_IMAGENET_PRETRAINED = True # otherwise use detectron, but that doesnt seem to work?!?
+
+# Change these to match where your annotations and images are
+
+VCR_IMAGES_DIR = ''
+if not os.path.exists(VCR_IMAGES_DIR):
+    raise ValueError("Update config.py with where you saved VCR images to.")
+
+VCR_FEATURES_DIR = ''
